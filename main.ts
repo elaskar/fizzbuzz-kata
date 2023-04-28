2444182e155347e94eebbe88b2818fe7a1ab24c6
Etienne Laskar <e.laskar@outlook.com>
2023-04-28 22:16:44 +0200

    chore: remove commander

--- main.ts
+++ main.ts
@@ -1,32 +1,40 @@
 #!/usr/bin/env node
 
-import { Command } from "commander";
-import { IllegalArgumentError } from "./src/IllegalArgumentError";
 import { fizzbuzzList } from "./src/fizzbuzzList";
 
-const program = new Command();
-
-program
-  .version("1.0.0")
-  .description("Fizzbuzz")
-  .addCommand(
-    new Command("fizzbuzz")
-      .argument("<maxValue>", "fizzbuzz upper bound")
-      .action(async (upperBound) => {
-        if (Number.isNaN(upperBound)) {
-          throw new IllegalArgumentError("Upper bound is not a number");
-        }
-        try {
-          fizzbuzzList(Number(upperBound));
-        } catch (err) {
-          console.error(err);
-          process.exit(1);
-        }
-      })
-  );
-
-async function main() {
-  await program.parseAsync();
+function main() {
+  if (process.argv.length < 4) {
+    console.error("missing arguments");
+    help();
+    process.exit(1);
+  }
+  if (process.argv.length > 4) {
+    console.error("too much arguments");
+    help();
+    process.exit(1);
+  }
+  if (process.argv[2] !== "fizzbuzz") {
+    console.error("incorrect function name");
+    help();
+    process.exit(1);
+  }
+  const maxValue = Number(process.argv[3]);
+  if (Number.isNaN(maxValue)) {
+    console.error("Upper bound is not a number");
+    help();
+    process.exit(1);
+  }
+  try {
+    fizzbuzzList(Number(maxValue));
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 }
-
 main();
+
+function help() {
+  console.log("## Program should be used by running : ");
+  console.log("## npm exec fizzbuzz <upperBound>");
+  console.log("## Upper bound should be a positive integer");
+}
